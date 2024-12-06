b44a32f23395d7dbbf4798c573cdac1e54b05822
ELENI VLIGOURIDOU <eleni.bligouridou@gmail.com>
2024-12-06 17:22:03 +0200

    Smatch file

--- parse_ready/Validation_penman.py
+++ parse_ready/Validation_penman.py
@@ -2,7 +2,7 @@ import penman
 
 def validate_amr_file(file_path):
     
-    with open('Mapped_GreekAnnotations.txt', 'r', encoding='utf-8') as file:
+    with open('map_el.txt', 'r', encoding='utf-8') as file:
         # Split annotations by double newline
         annotations = file.read().strip().split("\n\n")
 
@@ -19,4 +19,19 @@ def validate_amr_file(file_path):
             print(f"Error: {e}\n")
             
 # Call the function with your file
-validate_amr_file("Mapped_GreekAnnotations.txt")
+validate_amr_file("map_el.txt")
+
+
+import penman
+
+def check_for_duplicate_nodes(amr_text):
+    try:
+        graph = penman.decode(amr_text)
+        print("AMR is valid")
+    except Exception as e:
+        print(f"Invalid AMR: {e}")
+
+with open('map_cat.txt', 'r', encoding='utf-8') as f:
+    annotations = f.read().strip().split("\n\n")
+    for idx, annotation in enumerate(annotations, start=1):
+        check_for_duplicate_nodes(annotation)
